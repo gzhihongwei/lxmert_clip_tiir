ef3c785ca288bf4128fa26377c8bdc497114f516
George Wei <gzhihongwei@gmail.com>
2021-03-31 22:52:55 -0400

    Load image path instead of image itself

--- datasets/coco.py
+++ datasets/coco.py
@@ -1,29 +1,20 @@
 import cv2
 import os
 import random
-import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
 
 
 def get_loader(batch_size, root, ann_file="annotations/captions_train2014.json", 
-               transform=None, prob_unaligned=0.9, num_workers=0):
-    def _variable_image_sizes(batch):
-        captions = [item[0] for item in batch]
-        aligned = torch.tensor([item[1] for item in batch])
-        images = [item[2] for item in batch]
-        return [captions, aligned, images]
-    
+               prob_unaligned=0.9, num_workers=0):
     dataset = CoCoText2Img(root=root, 
                            ann_file=ann_file, 
-                           transform=transform,
                            prob_unaligned=prob_unaligned)
     
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batch_size,
                                   shuffle=True,
                                   num_workers=num_workers,
-                                  collate_fn=_variable_image_sizes,
                                   pin_memory=True)
     
     return data_loader
@@ -51,31 +42,28 @@ class CoCoText2Img(data.Dataset):
     def _load_caption(self, ann_id):
         return self.coco.loadAnns(ann_id)[0]["caption"]
     
-    def _load_image(self, image_id):
+    def _load_image_path(self, image_id):
         path = self.coco.loadImgs(image_id)[0]["file_name"]
-        return cv2.imread(path)
+        return os.path.join(self.root, path)
     
-    def _load_unaligned_image(self, true_image_id):
+    def _load_unaligned_image_path(self, true_image_id):
         candidate_image_ids = list({anns["image_id"] for anns in self.coco.anns.values() if anns["image_id"] != true_image_id})
         unaligned_image_id = random.choice(candidate_image_ids)
-        return self._load_image(unaligned_image_id)
+        return self._load_image_path(unaligned_image_id)
         
     
     def __getitem__(self, index):
         id = self.ids[index]
         caption = self._load_caption(id)
         image_id = self._get_caption_image_id(id)
-        image = self._load_image(image_id)
+        image = self._load_image_path(image_id)
         aligned = 1
         
         if random.random() < self.prob_unaligned:
             aligned = 0
-            image = self._load_unaligned_image(id)
+            image = self._load_unaligned_image_path(id)
             
-        if self.transform is not None:
-            image = self.transform(image)
-            
-        return caption, aligned, image
+        return caption, image, aligned
     
     def __len__(self):
         return len(self.ids)
