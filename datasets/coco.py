7a0d0c14e020f8d73fc01802c2d502744915b52e
George Wei <georgewei369@yahoo.com>
2021-03-31 22:32:42 -0400

    Allow for different sizes for images by adding a collate function

--- datasets/coco.py
+++ datasets/coco.py
@@ -1,18 +1,19 @@
+import cv2
 import os
 import random
+import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
 
-from PIL import Image
-
-
-IMAGE_TRANSFORM = transforms.Compose([
-    transforms.Resize((225, 225)),
-    transforms.ToTensor()
-])
 
 def get_loader(batch_size, root, ann_file="annotations/captions_train2014.json", 
                transform=None, prob_unaligned=0.9, num_workers=0):
+    def _variable_image_sizes(batch):
+        captions = [item[0] for item in batch]
+        aligned = torch.tensor([item[1] for item in batch])
+        images = [item[2] for item in batch]
+        return [captions, aligned, images]
+    
     dataset = CoCoText2Img(root=root, 
                            ann_file=ann_file, 
                            transform=transform,
@@ -22,6 +23,7 @@ def get_loader(batch_size, root, ann_file="annotations/captions_train2014.json",
                                   batch_size=batch_size,
                                   shuffle=True,
                                   num_workers=num_workers,
+                                  collate_fn=_variable_image_sizes,
                                   pin_memory=True)
     
     return data_loader
@@ -51,7 +53,7 @@ class CoCoText2Img(data.Dataset):
     
     def _load_image(self, image_id):
         path = self.coco.loadImgs(image_id)[0]["file_name"]
-        return Image.open(os.path.join(self.root, path)).convert("RGB")
+        return cv2.imread(path)
     
     def _load_unaligned_image(self, true_image_id):
         candidate_image_ids = list({anns["image_id"] for anns in self.coco.anns.values() if anns["image_id"] != true_image_id})
@@ -73,10 +75,7 @@ class CoCoText2Img(data.Dataset):
         if self.transform is not None:
             image = self.transform(image)
             
-        # Faster R-CNN expects BGR instead of RGB
-        image = image[..., ::-1]
-            
-        return caption, image, aligned
+        return caption, aligned, image
     
     def __len__(self):
         return len(self.ids)
@@ -86,8 +85,7 @@ if __name__ == "__main__":
     os.chdir(os.path.join("..", "..", "datasets", "coco"))
     train_loader = get_loader(batch_size=32,
                               root=os.path.join("images", "train2014"),
-                              ann_file=os.path.join("annotations", "captions_train2014.json"),
-                              transform=IMAGE_TRANSFORM)
+                              ann_file=os.path.join("annotations", "captions_train2014.json"))
     print(next(iter(train_loader)))
     
     
