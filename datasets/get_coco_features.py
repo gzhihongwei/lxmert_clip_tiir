1d8110f2cb54857e14864c92ee4f38b540a5ca96
George Wei <gzhihongwei@gmail.com>
2021-04-02 16:22:31 -0400

    Script that obtains features for COCO images

---
+++ datasets/get_coco_features.py
@@ -0,0 +1,71 @@
+import argparse
+import os
+import numpy as np
+
+from pycocotools.coco import COCO
+from generate_features import parse_args, load_model, get_detections_from_im
+from tqdm import tqdm
+
+
+def featurize_coco(args):
+    # Make sure the split is one in COCO 2014
+    assert args.split in {'train2014', 'val2014', 'test2014'}, 'split must be either "train2014", "val2014", or "test2014"'
+    
+    # Get the associated annotation file
+    if args.split == 'train2014':
+        ann_file = 'annotations/captions_train2014.json'
+    elif args.split == 'val2014':
+        ann_file = 'annotations/captions_val2014.json'
+    elif args.split == 'test2014':
+        ann_file = 'annotations/image_info_test2014.json'
+        
+    # Create COCO API for given annotation file
+    coco = COCO(ann_file)
+    
+    print('Loaded COCO annotation file')
+    
+    # Get image ids and filenames
+    image_ids = coco.getImgIds()
+    image_filenames = [image[0]['filename'] for image in coco.loadImgs(image_ids)]
+    
+    # Create the place to store the features extracted from Faster-RCNN
+    faster_dir = os.path.join('faster_features', args.split)
+    os.makedirs(faster_dir, exist_ok=True)
+    
+    print('Created directories to dump to')
+    
+    # Load the pre-trained model
+    faster_args = parse_args()
+    classes, faster_rcnn = load_model(faster_args)
+    
+    # Featurize each image in the COCO split
+    for image_filename, image_id in tqdm(zip(image_filenames, image_ids)):
+        detections = get_detections_from_im(faster_rcnn, classes, os.path.join(args.img_root, image_filename), image_id, faster_args)
+        np.save(os.path.join(faster_dir, f'{image_id}.npz'), **detections)
+        
+    print('Done obtaining all Faster-RCNN detections')
+        
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='Obtains Faster-RCNN pooled features and bounding boxes for COCO 2014 splits')
+    args.add_argument('-c', '--chdir', type=str, 
+                      dest='change_dir', default=os.path.join("..", "..", "datasets", "coco")
+                      help='Path to change to before featurizing')
+    args.add_argument('-s', '--split', type=str, 
+                      dest='split', required=True,
+                      help='Which split to featurize the images of')
+    args.add_argument('-i', '--image_root', type=str,
+                      dest='image_root', required=True,
+                      help='Relative path from change_dir')
+    
+    args.parse_args()
+    
+    os.chdir(args.change_dir)
+    featurize_coco(args)
+        
+        
+        
+    
+    
+    
+    
