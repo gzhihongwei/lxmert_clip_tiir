909c632d4c9080d2f3447d7714ba61eebaa53d56
George Wei <georgewei369@yahoo.com>
2021-04-18 22:35:15 -0400

    Added handling for Karpathy et al. split of COCO

--- datasets/coco.py
+++ datasets/coco.py
@@ -4,42 +4,159 @@ import random
 import torch.utils.data as data
 
 
-def get_loader(batch_size, feature_root, ann_file="annotations/captions_train2014.json", 
-               prob_unaligned=0.9, num_workers=0):
+def get_paths(path, use_restval=False):
     """
-    Returns the pytorch dataset for either the training or validation split of COCO 2014 captions
+    Returns paths to images and annotations for the MSCOCO dataset. The indices 
+    are extracted from the Karpathy et al. splits. Special thanks to Faghri for
+    the code.
     
-    :param batch_size: batch size of dataloader
-    :param feature_root: directory where all of the saved Faster RCNN features are located
-    :param ann_file: annotation file for the data to be loaded
-    :param prob_unaligned: probability that the Faster RCNN feature is not from the image
-                           paired with the caption
-    :param num_workers: number of other sub processes loading the data
-    :return: torch.utils.data.DataLoader for the COCO dataset
+    :param path: path to MSCOCO root
+    :param use_restval: if True, then the `restval` data is included in train.
+    :return: dictionary of paths and dictionary of the caption ids
     """
     
-    # Gets the pytorch dataset
-    dataset = CoCoText2Img(feature_root=feature_root, 
-                           ann_file=ann_file, 
-                           prob_unaligned=prob_unaligned)
+    # Stores the image roots and annotation file
+    roots = {}
+    # Stores the caption ids
+    ids = {}
     
-    # Gets the corresponding pytorch dataloader
+    # Path to the Faster RCNN extracted features
+    faster_dir = os.path.join(path, 'faster_features')
+    # Path to the annotations directory
+    capdir = os.path.join(path, 'annotations')
+    
+    # Paths for the regular training split
+    roots['train'] = {
+        'img': os.path.join(faster_dir, 'train2014'),
+        'cap': os.path.join(capdir, 'captions_train2014.json')
+    }
+    
+    # Paths for the regular validation split
+    roots['val'] = {
+        'img': os.path.join(faster_dir, 'val2014'),
+        'cap': os.path.join(capdir, 'captions_val2014.json')
+    }
+    
+    # Paths for the regular test split
+    roots['test'] = {
+        'img': os.path.join(faster_dir, 'val2014'),
+        'cap': os.path.join(capdir, 'captions_val2014.json')
+    }
+    
+    # Paths for the 'restval' split defined in Karpathy et al.
+    roots['trainrestval'] = {
+        'img': (roots['train']['img'], roots['val']['img']),
+        'cap': (roots['train']['cap'], roots['val']['cap'])
+    }
+    
+    # Caption ids for each split
+    ids['train'] = np.load(os.path.join(capdir, 'coco_train_ids.npy'))
+    ids['val'] = np.load(os.path.join(capdir, 'coco_dev_ids.npy'))[:5000]
+    ids['test'] = np.load(os.path.join(capdir, 'coco_test_ids.npy'))
+    # Combines 'restval' caption ids with that of train
+    ids['trainrestval'] = (
+        ids['train'],
+        np.load(os.path.join(capdir, 'coco_restval_ids.npy')))
+    
+    # Should training split include 'restval'?
+    if use_restval:
+        roots['train'] = roots['trainrestval']
+        ids['train'] = ids['trainrestval']
+        
+    return roots, ids
+
+
+def get_loader_single(root, json, prob_unaligned, batch_size=100,
+                      shuffle=True, num_workers=2, ids=None):
+    """
+    Returns torch.utils.data.DataLoader for custom COCO dataset.
+    
+    :param root: path to coco directory
+    :param json: path to annotation file
+    :param prob_unaligned: probability that the image-caption is aligned
+    :param batch_size: batch size for dataloader
+    :param shuffle: whether to shuffle the batches in the dataloader
+    :param num_workers: number of subprocesses helping loading data
+    :param ids: caption ids to consider
+    :return: torch.utils.data.DataLoader of the specified split of COCO
+    """
+    # COCO custom dataset
+    dataset = CoCoDataset(root=root,
+                          json=json,
+                          prob_unaligned=prob_unaligned,
+                          ids=ids)
+    
+    # Data loader
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batch_size,
-                                  shuffle=True,
-                                  num_workers=num_workers,
-                                  pin_memory=True)
-    
+                                  shuffle=shuffle,
+                                  pin_memory=True,
+                                  num_workers=num_workers)
     return data_loader
 
+def get_loaders(batch_size, workers, opt):
+    """
+    Returns the training PyTorch Dataloader for COCO.
+    
+    :param batch_size: batch size
+    :param workers: number of workers for the dataloader
+    :param opt: parsed arguments from argparse
+    :return: torch.utils.data.DataLoader for the training split (might include
+             'restval' based on opt)
+    """
+    
+    # Path to coco directory
+    dpath = os.path.join(opt.data_path, 'coco')
+    # Build the paths to everything
+    roots, ids = get_paths(dpath, opt.use_restval)
 
-class CoCoText2Img(data.Dataset):
+    # Get the loaders
+    train_loader = get_loader_single(roots['train']['img'],
+                                     roots['train']['cap'],
+                                     opt.prob_unaligned,
+                                     ids=ids['train'],
+                                     batch_size=batch_size,
+                                     shuffle=True,
+                                     num_workers=workers)
+
+    return train_loader
+
+
+def get_test_loader(split_name, batch_size, workers, opt):
+    """
+    Returns the test PyTorch Dataloader for COCO.
+    
+    :param batch_size: batch size
+    :param workers: number of workers for the dataloader
+    :param opt: parsed arguments from argparse
+    :return: torch.utils.data.DataLoader
+    """
+    
+    # Sanity check
+    assert split_name in {'val', 'test'}, 'Evaluation split must be either validation or test'
+    
+    # Path to coco directory
+    dpath = os.path.join(opt.data_path, 'coco')
+    # Build the paths to everything
+    roots, ids = get_paths(dpath, opt.use_restval)
+
+    # Get the loader
+    test_loader = get_loader_single(roots[split_name]['img'],
+                                    roots[split_name]['cap'],
+                                    0, ids=ids[split_name],
+                                    batch_size=batch_size,
+                                    shuffle=False,
+                                    num_workers=workers)
+    return test_loader
+
+
+class CoCoDataset(data.Dataset):
     """
     Pytorch dataset that handles COCO train2014 and val2014 splits
     """
-    def __init__(self, feature_root, ann_file, prob_unaligned):
+    def __init__(self, root, json, prob_unaligned, ids):
         """
-        Constructor for CoCoText2Img
+        Constructor for CoCoDataset
         
         :param feature_root: directory where all of the saved Faster RCNN features are located
         :param ann_file: annotation file for the data to be loaded
@@ -48,42 +165,40 @@ class CoCoText2Img(data.Dataset):
         """
         from pycocotools.coco import COCO
         
-        # Store the feature root
-        self.feature_root = feature_root
-        # Create a COCO instance
-        self.coco = COCO(ann_file)
-        # All of the caption ids
-        self.ids = list(sorted(self.coco.anns.keys()))
+        # Store the root
+        self.root = root
+        
+        # When using the 'restval' split, two json files are needed
+        if isinstance(json, tuple):
+            self.coco = (COCO(json[0]), COCO(json[1]))
+        else:
+            self.coco = (COCO(json),)
+            self.root = (root,)
+         
+        # Caption ids from get_paths   
+        self.ids = ids
+        
+        # If 'restval' data is used, record the break point for the ids
+        if isinstance(self.ids, tuple):
+            self.bp = len(self.ids[0])
+            self.ids = list(self.ids[0]) + list(self.ids[1])
+        else:
+            self.bp = len(self.ids)
+        
         # Probability of returning an unaligned image from caption
         assert 0 <= prob_unaligned <= 1, "prob_unaligned must be a probability" 
         self.prob_unaligned = prob_unaligned
-        
-    def _get_caption_image_id(self, ann_id):
-        """
-        Gets the image_id of the image paired with caption of ann_id
-        
-        :param ann_id: caption id in COCO
-        :return: int the image_id of the associated image to the caption
-        """
-        return self.coco.loadAnns(ann_id)[0]["image_id"]
     
-    def _load_caption(self, ann_id):
-        """
-        Gets the caption with ann_id
-        
-        :param ann_id: caption id in COCO
-        :return: str the caption
-        """
-        return self.coco.loadAnns(ann_id)[0]["caption"]
-    
-    def _load_image_features(self, image_id):
+    @staticmethod
+    def _load_image_features(root, image_id):
         """
         Loads the bounding boxes and features extracted from Faster RCNN for LXMERT
         
+        :param root: path to feature
         :param image_id: image id in COCO
         :return: Tuple[np.ndarray, np.ndarray] normalized bounding boxes and ROI pooled features
         """
-        def _reform_faster_features(saved):
+        def __reform_faster_features(saved):
             """
             Unpacks data from loaded .npz file and prepares it for LXMERT format
             
@@ -109,22 +224,33 @@ class CoCoText2Img(data.Dataset):
             return reformed_boxes, features
             
         # Load cached features
-        faster_features = np.load(os.path.join(self.feature_root, f'{image_id}.npz'))
-        return _reform_faster_features(faster_features)
+        faster_features = np.load(os.path.join(root, f'{image_id}.npz'))
+        return __reform_faster_features(faster_features)
     
-    def _load_unaligned_features(self, true_image_id):
+    def _load_unaligned_features(self, true_img_id):
         """
         Randomly loads a different image's features than that of true_image_id
         
+        :param root: 
         :param true_image_id: image id in COCO to avoid loading of
         :return: Tuple[np.ndarray, np.ndarray] Faster RCNN features of image that is not true_image_id
         """
         
         # All of the possible image_ids to sample
-        candidate_image_ids = list({anns["image_id"] for anns in self.coco.anns.values() if anns["image_id"] != true_image_id})
-        # Sample an image_id
-        unaligned_image_id = random.choice(candidate_image_ids)
-        return self._load_image_features(unaligned_image_id)
+        candidate_img_ids1 = list({anns["image_id"] for anns in self.coco[0].anns.values() if anns["image_id"] != true_img_id})
+        bp = len(candidate_img_ids1)
+        
+        # If 'retval' is being used or not
+        if len(self.coco) == 2:
+            candidate_img_ids2 = list({anns["image_id"] for anns in self.coco[1].anns.values() if anns["image_id"] != true_img_id})
+            candidate_img_ids = candidate_img_ids1 + candidate_img_ids2
+        else:
+            candidate_img_ids = candidate_img_ids1
+            
+        # Sample a random index
+        idx = random.choice(range(len(candidate_img_ids)))
+        root = self.root[0] if idx < bp else self.root[1]
+        return self._load_image_features(root, candidate_img_ids[idx])
         
     
     def __getitem__(self, index):
@@ -135,20 +261,30 @@ class CoCoText2Img(data.Dataset):
         :return: Tuple[str, ]
         """
         
-        # Get the caption id at index of the list of caption ids
-        id = self.ids[index]
+        # If the index is less than the breakpoint, it is part of the 
+        # original training split.
+        if index < self.bp:
+            coco = self.coco[0]
+            root = self.root[0]
+        else:
+            coco = self.coco[1]
+            root = self.root[1]
+            
+        # Annotation id to be retrieved
+        ann_id = self.ids[index]
+        
         # Load corresponding COCO data
-        caption = self._load_caption(id)
-        image_id = self._get_caption_image_id(id)
-        boxes, features = self._load_image_features(image_id)
+        caption = coco.anns[ann_id]['caption']
+        img_id = coco.anns[ann_id]['image_id']
+        boxes, features = self._load_image_features(root, img_id)
         aligned = 1
         
         # Randomly unalign pairing with probability of prob_unaligned
         if random.random() < self.prob_unaligned:
+            boxes, features = self._load_unaligned_features(root, img_id)
             aligned = 0
-            image = self._load_unaligned_features(id)
             
-        return caption, image, aligned
+        return caption, boxes, features, aligned, index, img_id
     
     def __len__(self):
         """
@@ -157,14 +293,3 @@ class CoCoText2Img(data.Dataset):
         :return: int length of dataset
         """
         return len(self.ids)
-        
-        
-if __name__ == "__main__":
-    os.chdir(os.path.join("..", "..", "datasets", "coco"))
-    train_loader = get_loader(batch_size=32,
-                              root=os.path.join("images", "train2014"),
-                              ann_file=os.path.join("annotations", "captions_train2014.json"))
-    print(next(iter(train_loader)))
-    
-    
-  
