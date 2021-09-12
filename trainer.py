744d001b698f028288df7f377c4a5a9d37bf3379
George Wei <georgewei369@yahoo.com>
2021-09-12 14:03:16 -0400

    Beginning integration with Huggingface Trainer

---
+++ trainer.py
@@ -0,0 +1,208 @@
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional, Dict, Union, Any
+
+from datasets.coco import get_test_dataset, get_train_dataset
+from models.lxmert import LxmertForTBIR
+
+import torch
+import torch.nn as nn
+from torch.cuda.amp import autocast
+
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    HfArgumentParser,
+    set_seed,
+    Trainer,
+    TrainingArguments
+)
+from transformers.trainer_utils import get_last_checkpoint
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where to store the pretrained models downloaded from huggingface.co"},
+    )
+    use_fast_tokenizer: bool = field(
+        default=True,
+        metadata={"help": "Whether to use one of the fast tokenizer (backed by the tokenizers library) or not."},
+    )
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+    
+    data_path: str = field(
+        metadata={"help": "Path to the data directory that has COCO."}
+    )
+    prob_unaligned: float = field(
+        metadata={"help": "Probability that the images for each caption are randomly sampled from the negative images."}
+    )
+    use_restval: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Whether to use the restval split from Karpathy et al. in training."}
+    )
+    
+
+class LxmertForTBIRTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        print(inputs)
+        return
+        outputs = model(**inputs)
+
+        if self.args.past_index >= 0:
+            self._past = outputs[self.args.past_index]
+
+        loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+
+        return (loss, outputs) if return_outputs else loss
+        
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+        
+        if self.use_amp:
+            with autocast():
+                loss = self.compute_loss(model, inputs)
+        else:
+            loss = self.compute_loss(model, inputs)
+            
+        if self.args.n_gpu > 1:
+            loss = loss.mean() # mean() to average on multi-gpu parallel training
+            
+        if self.args.gradient_accumulation_steps > 1 and not self.deepspeed:
+            loss /= self.args.gradient_accumulation_steps
+            
+        if self.use_amp:
+            self.scaler.scale(loss).backward()
+        elif self.deepspeed:
+            loss = self.deepspeed.backward(loss)
+        else:
+            loss.backward()
+        
+        return loss.detach()
+                
+            
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+        
+    log_level = training_args.get_process_log_level()
+    logger.setLevel(log_level)
+    
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+    logger.info(f"Training/evaluation parameters {training_args}")
+    
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+            
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+    
+    train_dataset = get_train_dataset(data_args)
+    val_dataset = get_test_dataset("val", data_args)
+    test_dataset = get_test_dataset("test", data_args)
+    
+    # Load pretrained model and tokenizer
+    #
+    # Distributed training:
+    # The .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+    config = AutoConfig.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+        use_fast=model_args.use_fast_tokenizer,
+    )
+    model = LxmertForTBIR.from_pretrained(
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+        cache_dir=model_args.cache_dir,
+    )
+    
+    model.resize_token_embeddings(len(tokenizer))
+    
+    if not training_args.do_train and not (training_args.do_eval or training_args.do_predict):
+        logger.info("There is nothing to do. Please pass `do_train`, `do_eval` and/or `do_predict`.")
+        return
+    
+    # Initialize our Trainer
+    trainer = LxmertForTBIRTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset if training_args.do_train else None,
+        eval_dataset=val_dataset if training_args.do_eval else test_dataset if training_args.do_predict else None,
+        tokenizer=tokenizer,
+        # compute_metrics=compute_metrics if training_args.predict_with_generate else None,
+    )
+    
+    # Training
+    if training_args.do_train:
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+        
+    if training_args.do_eval or training_args.do_predict:
+        logger.info("*** Evaluate ***")
+        split = "val" if training_args.do_eval else "test"
+        metrics = trainer.evaluate(metric_key_prefix=split)
+
+        trainer.log_metrics(split, metrics)
+        trainer.save_metrics(split, metrics)
+
+
+if __name__ == "__main__":
+    main()
