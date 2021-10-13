169d6dfcd60462dda36d3696c46c073c419ed9db
George Wei <georgewei369@yahoo.com>
2021-10-13 11:02:08 -0400

    Implements contrastive loss

--- information_retrieval/coco_ir.py
+++ information_retrieval/coco_ir.py
@@ -154,14 +154,36 @@ class RetrievalDataset(Dataset):
         return len(self.img_keys) * self.num_captions_per_img
 
 
-def compute_score_with_logits(logits, labels):
-    if logits.shape[1] > 1:
-        logits = torch.max(logits, 1)[1].data # argmax
-        scores = logits == labels 
-    else:
-        scores = torch.zeros_like(labels).cuda()
-        for i, (logit, label) in enumerate(zip(logits, labels)):
-            logit_ = torch.sigmoid(logit)
-            if (logit_ >= 0.5 and label == 1) or (logit_ < 0.5 and label == 0):
-                scores[i] = 1
-    return scores
+class ContrastiveLoss(nn.Module):
+    """
+    Compute contrastive loss
+    """
+
+    def __init__(self, margin: int = 0, max_violation: bool = False):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.max_violation = max_violation
+
+    def forward(self, scores: torch.tensor) -> torch.tensor:
+        diagonal = scores.diag().view(scores.size(0), 1)
+        d1 = diagonal.expand_as(scores)
+        d2 = diagonal.t().expand_as(scores)
+
+        # compare every diagonal score to scores in its column
+        # caption retrieval
+        cost_s = (self.margin + scores - d1).clamp(min=0)
+        # compare every diagonal score to scores in its row
+        # image retrieval
+        cost_im = (self.margin + scores - d2).clamp(min=0)
+
+        # clear diagonals
+        mask = torch.eye(scores.size(0)) > .5
+        cost_s = cost_s.masked_fill(mask, 0)
+        cost_im = cost_im.masked_fill(mask, 0)
+
+        # keep the maximum violating negative for each query
+        if self.max_violation:
+            cost_s = cost_s.max(1)[0]
+            cost_im = cost_im.max(0)[0]
+
+        return cost_s.sum() + cost_im.sum()
