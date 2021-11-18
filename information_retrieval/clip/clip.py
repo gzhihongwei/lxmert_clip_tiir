bc321cb8f1ab55c6cc2e644136283e0df7b67cf6
gzhihongwei <georgewei369@yahoo.com>
2021-11-18 10:40:23 -0500

    Fixed bugs with output and changed format to work for Trainer

--- information_retrieval/clip/clip.py
+++ information_retrieval/clip/clip.py
@@ -2,7 +2,8 @@ from dataclasses import dataclass
 
 import torch
 
-from transformers import CLIPModel, CLIPOutput
+from transformers import CLIPModel
+from transformers.models.clip.modeling_clip import CLIPOutput
 
 
 @dataclass
@@ -17,11 +18,14 @@ class CLIPForIR(CLIPModel):
         pixel_values=None,
         attention_mask=None,
         position_ids = None,
-        return_loss=None,
+        labels=None,
         output_attentions=None,
         output_hidden_states=None,
         return_dict=None,
     ):
+        return_loss = labels is not None
+        return_dict = return_dict if return_dict is not None else self.config.return_dict
+
         clip_output = super().forward(input_ids=input_ids, 
                                       pixel_values=pixel_values, 
                                       attention_mask=attention_mask, 
@@ -31,10 +35,10 @@ class CLIPForIR(CLIPModel):
                                       output_hidden_states=output_hidden_states,
                                       return_dict=return_dict)
         if not return_dict:
-            matching_score = torch.diagonal(clip_output[0 if not return_loss else 1])
-            return (clip_output[0], matching_score) + clip_output[1:] if return_loss else (matching_score,) + clip_output
+            matching_score = torch.diagonal(clip_output[0 if not return_loss else 1]).contiguous()
+            return (clip_output[0], matching_score) if return_loss else matching_score
         
-        matching_score = torch.diagonal(clip_output.logits_per_image)
+        matching_score = torch.diagonal(clip_output.logits_per_image).contiguous()
         
         return CLIPForIROutput(
             loss=clip_output.loss,
@@ -43,6 +47,6 @@ class CLIPForIR(CLIPModel):
             logits_per_text=clip_output.logits_per_text,
             text_embeds=clip_output.text_embeds,
             image_embeds=clip_output.image_embeds,
-            text_model_output=clip_output.text_model_outputs,
+            text_model_output=clip_output.text_model_output,
             vision_model_output=clip_output.vision_model_output,
         )
